3b577c33527eb251cc013bc11f07ead73dfc15ca
Kiselev777 <forzorezor1@mail.ru>
2019-06-03 09:12:14 +0300

    1.0.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,109 +7,25 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Random;
-
 public class Main extends Application {
-    private boolean flag = false;
-    private Battle enemyBattle, myBattle;
-    private int countShip = 10;
-    private boolean eTurn;
-    private int[] deck = {1, 1, 1, 1, 2, 2, 2, 3, 3, 4};
-
-
-    private void computer() throws InterruptedException {
-        while (eTurn) {
-            Random rand = new Random();
-            int x = rand.nextInt(10);
-            int y = rand.nextInt(10);
-            Cell cell = myBattle.getCell(x, y);
-            if (cell.isShoot)
-                continue;
-            eTurn = cell.damage();
-            if (myBattle.countShip == 0) {
-                System.out.println("Проиграл...");
-                Thread.sleep(1000);
-                System.exit(0);
-            }
-        }
-    }
-
-    private void computerPlace() {
-        int type = 9;
-        Random rand = new Random();
-        while (type >= 0) {
-            if (enemyBattle.placeShip(new Ship(deck[type], Math.random() < 0.5), rand.nextInt(10), rand.nextInt(10))) {
-                type--;
-            }
-        }
-
-        flag = true;
-    }
-
-    private Parent effect() throws IOException {
-        BorderPane pane = new BorderPane();
-        pane.setPrefSize(800, 800);
-        enemyBattle = new Battle(true, e -> {
-            Cell cell = (Cell) e.getSource();
-            if (!flag)
-                return;
-            if (cell.isShoot)
-                return;
-            eTurn = !cell.damage();
-            if (eTurn) {
-                try {
-                    computer();
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-            }
-            if (enemyBattle.countShip == 0) {
-                System.out.println("ЭТО ПОБЕДА");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                System.exit(0);
-            }
-
-        });
-        myBattle = new Battle(false, e -> {
-            if (flag)
-                return;
-            Cell cell = (Cell) e.getSource();
-            if (myBattle.placeShip(new Ship(deck[countShip - 1], e.getButton() == MouseButton.PRIMARY), cell.x, cell.y)) {
-                if (--countShip == 0) {
-                    computerPlace();
-                    pane.setRight(new Text("THE GAME IS START"));
-                }
-            }
-
-        });
-
-        VBox vbox = new VBox(75, myBattle, enemyBattle);
-        vbox.setAlignment(Pos.CENTER);
-        pane.setCenter(vbox);
-
-        return pane;
+    Logic log=new Logic();
+    private void setEffect(Logic lo) throws IOException {
+        lo.effect();
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        setEffect(log);
         StackPane pane = new StackPane();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         pane.getChildren().add(root);
-        pane.getChildren().add(effect());
+        pane.getChildren().add(log.setPane());
         Scene sc = new Scene(pane);
         primaryStage.setTitle("Battleship");
         primaryStage.setScene(sc);
